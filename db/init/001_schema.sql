280356706c5826f98c4b89927022dea960c06fd1
HatfanSahrul1 <hatfansahrul1@gmail.com>
2025-10-29 18:24:47 +0700

    refactor : change database scheme

--- db/init/001_schema.sql
+++ db/init/001_schema.sql
@@ -1,6 +1,17 @@
-CREATE TABLE IF NOT EXISTS scores (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  player VARCHAR(100),
-  score INT,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+create table if not exist db_user (
+    id int auto_increment primary key,
+    username varchar(50) not null unique,
+    password_hash varchar(255) not null,
+    created_at timestamp default current_timestamp
+);
+
+create table if not exists db_attributes (
+    id int auto_increment primary key,
+    player_id int,
+    score int,
+    coin int,
+    green_skin tinyint(1) default 0,
+    red_skin tinyint(1) default 0,
+    blue_skin tinyint(1) default 0,
+    foreign key (player_id) references db_user(id)
 );
